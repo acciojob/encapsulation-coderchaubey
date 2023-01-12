9b237d20f0c550aef4369eacf5705d2ab23ef73a
coderchaubey <sunnychaubeysm@gmail.com>
2023-01-13 01:17:06 +0530

    encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+    RWOnly obj=new RWOnly();
+    // error: 'name' has private access in 'com.driver.RWOnly'
+//    obj.name="what";
+
+        //error: 'name' has private access in 'com.driver.RWOnly'
+//        System.out.println(obj.name);
+        obj.setName("Chaubey");
+        System.out.println(obj.getName());
+    }
 }
